31c0b50ce0af657ec548ee444da67e376db609e6
salma-djadi <salma.djadi@epitech.eu>
2022-01-30 10:46:05 +0100

    reister login & logout

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './components/Main';
+import store from './store';
+import {loadUser} from './actions/authActions';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    store.dispatch(loadUser());
+  }
+  render(){
+    return ( 
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main/>
+          </div> 
+        </BrowserRouter>
+        </Provider> 
+    );
+  }
 }
 
 export default App;
