5d7c6d35d5c22f7f42c873a5847ff040c0b235ac
protechcode <protechcode01@gmail.com>
2022-02-01 16:12:54 +0100

    separate components like react

---
+++ my-app/src/components/ecommerce.js
@@ -0,0 +1,131 @@
+import bluejacket from '../images/blue-sky-jaket.jpg';
+
+function ecommerce() {
+    return(
+        <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-4">
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img className="w-full h-full lg:max-w-2xl" src={bluejacket} alt="Catalogue-pana.svg" />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <p class="mt-1">16.00€</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img className="w-full h-full lg:max-w-2xl" src={bluejacket} />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <p class="mt-1">21.15€</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img className="w-full h-full lg:max-w-2xl" src={bluejacket} alt="Catalogue-pana.svg" />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <p class="mt-1">12.00€</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img className="w-full h-full lg:max-w-2xl" src={bluejacket} alt="Catalogue-pana.svg" />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <p class="mt-1">18.40€</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img className="w-full h-full lg:max-w-2xl" src={bluejacket} alt="Catalogue-pana.svg" />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <p class="mt-1">16.00€</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img className="w-full h-full lg:max-w-2xl" src={bluejacket} alt="Catalogue-pana.svg" />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <p class="mt-1">21.15€</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img className="w-full h-full lg:max-w-2xl" src={bluejacket} alt="Catalogue-pana.svg" />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <p class="mt-1">12.00€</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img className="w-full h-full lg:max-w-2xl" src={bluejacket} alt="Catalogue-pana.svg" />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <p class="mt-1">18.40€</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+};
+export default ecommerce;
