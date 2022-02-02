325dda68517ef587e8877f3ee7186bd4a96df94d
salma-djadi <salma.djadi@epitech.eu>
2022-02-02 16:41:56 +0100

    correction item controller

--- Full-Energy/controllers/ItemController.js
+++ Full-Energy/controllers/ItemController.js
@@ -5,12 +5,13 @@ module.exports.get_items = (req,res) => {
 }
 
 module.exports.get_item = (req,res) => {
-    Item.findById({_id: req.params.id},req).then(function(item){
+    Item.findById({_id: req.params.id},req.body).then(function(item){
         Item.findOne({_id: req.params.id}).then(function(item){
             res.json(item);
         });
     });
 }
+
 
 module.exports.post_item = (req,res) => {
     const newItem = new Item(req.body);
